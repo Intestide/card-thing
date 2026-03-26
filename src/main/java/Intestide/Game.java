5ec6f10ca4206fde1456f1269f296be8f3408622
walmartKevin <yikpok.liu145550-bisz@basischina.com>
2026-03-26 11:29:38 +0800

    funny guy is here

--- src/main/java/Intestide/Game.java
+++ src/main/java/Intestide/Game.java
@@ -1,23 +1,40 @@
-public class Game 
-{
-    public static Cards[] deck = new Cardss[52];
-    public static Cards[] selected = new Cards[3];
-    public static Cards[][] grid = new Cards[3][3];
+package Intestide;
 
-    public void shuffle()
-    {
-        // paste shuffle Cards in
-    }
-    public void select(Cards card)
-    {
-        
-    }
-    public void replace()
-    {
+import java.util.Random;
 
+public class Game {
+  public static Cards[] deck = new Cards[52];
+  public static Cards[] selected = new Cards[3];
+  public static Cards[] grid = new Cards[9];
+
+  public Game() {
+    shuffle();
+    deal();
+  }
+
+  private void deal() {
+    for (byte i = 0; i < 9; i++) {
+      grid[i] = deck[i];
     }
-    public void deselectAll()
-    {
-        
+  }
+  public void shuffle() {
+    for (byte i = 0; i < 52; i++) {
+      deck[i] = new Cards(i);
     }
+    Random rand = new Random();
+    for (int i = 51; i > 0; i--) {
+      int j = rand.nextInt(i + 1);
+      Cards temp = deck[i];
+      deck[i] = deck[j];
+      deck[j] = temp;
+    }
+  }
+
+  public Cards[] getCards() {
+    return grid;
+  }
+
+  public int getBoardSize() {
+    return grid.length;
+  }
 }
