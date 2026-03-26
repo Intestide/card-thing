e1ee7d8094cbf1f2f4d9546506bf8008b00b9b48
davidthegiraffe <davidliziwei@gmail.com>
2026-03-26 03:11:41 +0000

    modified:   src/main/java/Intestide/Game.java

--- src/main/java/Intestide/Game.java
+++ src/main/java/Intestide/Game.java
@@ -1,3 +1,23 @@
-public class Game {
+public class Game 
+{
+    public static Cards[] deck = new Cardss[52];
+    public static Cards[] selected = new Cards[3];
+    public static Cards[][] grid = new Cards[3][3];
 
+    public void shuffle()
+    {
+        // paste shuffle Cards in
+    }
+    public void select(Cards card)
+    {
+        
+    }
+    public void replace()
+    {
+
+    }
+    public void deselectAll()
+    {
+        
+    }
 }
